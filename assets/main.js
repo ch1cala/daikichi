dc10b4b0c5be57b3ea93687d4a094a751021b63f
Chikara <chikara@energizekk.com>
2014-01-01 22:18:26 +0900

    add share links

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,17 @@
 angular.module('daikichiApp', [])
 
-  .controller('DrawCtrl', function($scope, $http){
+  .controller('DrawCtrl', function($scope, $http, $location){
     $scope.result = null;
+    $scope.shareText = '';
+    $scope.fbAppId = '640131609366815';
+//    $scope.url = encodeURIComponent($location.absUrl());
+    $scope.url = encodeURIComponent('http://daikichi.energize.io');
 
     $http.get('assets/results.json')
       .success(function(data){
         var i = Math.floor( Math.random() * data.length );
         $scope.result = data[i];
+        $scope.shareText = encodeURIComponent(data[i].text);
       });
 
   });
